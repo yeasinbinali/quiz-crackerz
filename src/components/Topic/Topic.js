0e8eeba476fbf3a2304e4aef559812c765171ed8
yeasinbinali <yeasinbinali@gmail.com>
2023-06-12 17:05:45 +0600

    topic updated

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -6,14 +6,14 @@ const Topic = (props) => {
   const { id, name, logo, total } = props.topic;
   return (
     <div class="col">
-      <div class="card h-100">
+      <div class="card h-80">
         <img src={logo} class="card-img-top" alt="..." />
         <div class="card-body">
           <div>
             <h5 class="card-title">{name}</h5>
             <p>Questions: {total}</p>
           </div>
-          <p><small><Link to={`/quiz/${id}`} className="topic-btn">Quiz -</Link></small></p>
+          <p><small><Link to={`/quiz/${id}`} className="topic-btn">Quiz</Link></small></p>
         </div>
       </div>
     </div>
